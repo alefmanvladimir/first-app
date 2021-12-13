bb6a910f35de965c364943b237a404b1b8a10b41
alefmanvladimir <smagazhevskijj@gmail.com>
2021-12-13 14:07:03 +0200

    fix

--- src/services/colledge.js
+++ src/services/colledge.js
@@ -8,35 +8,33 @@ export default class Colledge{
     }
 
     addCourse(course){
-        course.id = this.#getId()
+        
         if(!this.#validate(course)){
             return null
         }
-        
+        course.id = this.#getId()
         this.#coursesProvider.add(course)
     }
 
     #validate(course){
-        const checkId = course.id>=this.#courseData.minId && course.id <= this.#courseData.maxId
         const checkCost = course.cost>=this.#courseData.minCost && course.cost <= this.#courseData.maxCost
         const checkHours = course.hours>=this.#courseData.minHours && course.hours <= this.#courseData.maxHours
         const checkYear = course.openDate.getFullYear()>=this.#courseData.minYear && course.openDate.getFullYear() <= this.#courseData.maxYear
         
-        const checkName = this.#courseData.courseNames.findIndex(courseName => courseName === course.courseName)
-        const checkLecturer = this.#courseData.lecturers.findIndex(lecturerName => lecturerName === course.lecturerName)
-        const checkType = this.#courseData.types.findIndex(type => type === course.type)
-        const checkTiming = this.#courseData.timing.findIndex(dayEvening => dayEvening === course.dayEvening)
+        const checkName = this.#courseData.courseNames.find(courseName => courseName === course.courseName)
+        const checkLecturer = this.#courseData.lecturers.find(lecturerName => lecturerName === course.lecturerName)
+        const checkType = this.#courseData.types.find(type => type === course.type)
+        const checkTiming = this.#courseData.timing.find(dayEvening => dayEvening === course.dayEvening)
 
         let strError = ""
 
-            strError += !checkId?`incorrect id - ${course.id}\n`:''
             strError += !checkCost?`incorrect cost - ${course.cost}\n`:''
             strError += !checkHours?`incorrect hours - ${course.hours}\n`:''
             strError += !checkYear?`incorrect openDate - ${course.openDate}\n`:''
-            strError += checkName===-1?`incorrect course name - ${course.courseName}\n`:''
-            strError += checkLecturer===-1?`incorrect lecturer - ${course.lecturerName}\n`:''
-            strError += checkType===-1?`incorrect type of course - ${course.type}\n`:''
-            strError += checkTiming===-1?`incorrect timing - ${course.dayEvening}\n`:''
+            strError += !checkName?`incorrect course name - ${course.courseName}\n`:''
+            strError += !checkLecturer?`incorrect lecturer - ${course.lecturerName}\n`:''
+            strError += !checkType?`incorrect type of course - ${course.type}\n`:''
+            strError += !checkTiming?`incorrect timing - ${course.dayEvening}\n`:''
         
         if(strError){
             throw new Error(strError)
